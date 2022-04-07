91162528f5cc8fd39665c47ede7bbb2a552a555c
kwaktaemin <kwak9898@github.com>
2022-04-07 17:58:11 +0900

    React 상품 정보 게시물 구현

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+const productList = [
+  {
+    no: 1,
+    id: "첫번째 아이디",
+    productName: "제품1",
+    price: "10000",
+  },
+  {
+    no: 2,
+    id: "두번째 아이디",
+    productName: "제품2",
+    price: "10000",
+  },
+  {
+    no: 3,
+    id: "세번째 아이디",
+    productName: "제품3",
+    price: "10000",
+  },
+  {
+    no: 4,
+    id: "네번째 아이디",
+    productName: "제품4",
+    price: "10000",
+  },
+  {
+    no: 5,
+    id: "다섯번째 아이디",
+    productName: "제품5",
+    price: "10000",
+  },
+];
+
+const getProductNo = no => {
+  const array = productList.filter(x => x.no == no);
+  if (array.length == 1) {
+    return array[0];
+  }
+  return null;
+};
+
+export { productList, getProductNo };
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,50 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CommonTable from "../components/CommonTable";
+import CommonTableColumn from "../components/CommonTableColumn";
+import CommonTableRow from "../components/CommonTableRow";
+import { productList } from "../Data";
 
 const ProductList = props => {
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    setDataList(productList);
+  }, []);
+
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>아이디</th>
-            <th>제품명</th>
-            <th>가격</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>첫번째 아이디</td>
-            <td>제품1</td>
-            <td>10000</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>두번째 아이디</td>
-            <td>제품2</td>
-            <td>10000</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>세번째 아이디</td>
-            <td>제품3</td>
-            <td>10000</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>네번째 아이디</td>
-            <td>제품4</td>
-            <td>10000</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>다섯번째 아이디</td>
-            <td>제품5</td>
-            <td>10000</td>
-          </tr>
-        </tbody>
-      </table>
+      <CommonTable headersName={["글번호", "제목", "등록일", "조회수"]}>
+        {dataList
+          ? dataList.map((item, index) => {
+              return (
+                <CommonTableRow key={index}>
+                  <CommonTableColumn>{item.no}</CommonTableColumn>
+                  <CommonTableColumn>{item.id}</CommonTableColumn>
+                  <CommonTableColumn>{item.productName}</CommonTableColumn>
+                  <CommonTableColumn>{item.price}</CommonTableColumn>
+                </CommonTableRow>
+              );
+            })
+          : ""}
+      </CommonTable>
     </>
   );
 };
