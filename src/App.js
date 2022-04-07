f1441f47cb30e110de7120f846e9a06fd6613999
kwaktaemin <kwak9898@github.com>
2022-04-07 18:03:52 +0900

    상품 상세 view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import ProductMain from "./pages/ProductMain";
+import ProductView from "./pages/ProductView";
 
 function App() {
   return (
     <div className="App">
-      <ProductMain />
+      <BrowserRouter>
+        <Route exact path="/productView/:no" component={ProductView} />
+        <Route exact path="/" component={ProductMain} />
+      </BrowserRouter>
     </div>
   );
 }
