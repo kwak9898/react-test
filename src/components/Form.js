8531356b08464f8e58a914315a3f52fbed3f0aa1
kwaktaemin <kwak9898@github.com>
2022-04-08 14:23:06 +0900

    Product List Add Css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Form.css";
 
-const Form = props => {
+const Form = ({ value, onChange, onCreate, onKeyPress }) => {
   return (
-    <div>
-      <fieldset>
-        <legend>제품</legend>
-        <label for="id">제품명</label>
-        <input type="text" id="product-name" />
-        <br />
-        <label for="id">제품아이디</label>
-        <input type="text" id="id" />
-        <br />
-        <label for="id">가격</label>
-        <input type="text" id="price" />
-        <br />
-      </fieldset>
+    <div className="form">
+      <label className="title" for="product-name">
+        제품명
+      </label>
+      <input type="text" id="product-name" />
+      <label className="title" for="product-id">
+        제품아이디
+      </label>
+      <input type="text" id="product-id" />
+      <label className="title" for="product-price">
+        가격
+      </label>
+      <input type="text" id="product-price" />
+      <div className="create-button" onClick={onCreate}>
+        추가
+      </div>
     </div>
   );
 };
