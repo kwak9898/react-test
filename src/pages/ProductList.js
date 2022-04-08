a1c13221b6a2e5944318905f3c713712b0899892
kwaktaemin <kwak9898@github.com>
2022-04-08 09:52:58 +0900

    제품 List 수정

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -14,18 +14,18 @@ const ProductList = props => {
 
   return (
     <>
-      <CommonTable headersName={["제품번호", "아이디", "제품명", "가격"]}>
+      <CommonTable headersName={["제품번호", "제품명", "제품아이디", "가격"]}>
         {dataList
           ? dataList.map((item, index) => {
               return (
                 <CommonTableRow key={index}>
                   <CommonTableColumn>{item.no}</CommonTableColumn>
-                  <CommonTableColumn>{item.id}</CommonTableColumn>
                   <CommonTableColumn>
                     <Link to={`/productView/${item.no}`}>
                       {item.productName}
                     </Link>
                   </CommonTableColumn>
+                  <CommonTableColumn>{item.id}</CommonTableColumn>
                   <CommonTableColumn>{item.price}</CommonTableColumn>
                 </CommonTableRow>
               );
